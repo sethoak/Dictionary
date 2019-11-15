21613a69268803152ec11cc417f917e840f7d780
sethoak <sethoak@gmail.com>
2019-11-15 14:45:32 -0600

    saving code

--- Program.cs
+++ Program.cs
@@ -39,6 +39,98 @@ namespace Csharp_Common_Type_Dictionary
                 Console.WriteLine($"The word {word.Key} means {word.Value}");
 
             }
+
+            //NEW STUFF
+
+            // Make a new list
+            List<Dictionary<string, string>> dictionaryOfWords = new List<Dictionary<string, string>>();
+            /*
+                You want to track the following about each word:
+                    word, definition, part of speech, example sentence
+
+                Example of one dictionary in the list:
+                {
+                    "word": "excited",
+                    "definition": "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness",
+                    "part of speech": "adjective",
+                    "example sentence": "I am excited to learn C#!"
+                }
+            */
+
+            // Create dictionary to represent a few words
+            Dictionary<string, string> excitedWord = new Dictionary<string, string>()
+            {
+             {
+                    "word": "excited",
+                    "definition": "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness",
+                    "part of speech": "adjective",
+                    "example sentence": "I am excited to learn C#!"
+                },
+                {
+                    "word": "sad",
+                    "definition": "just feelin down, man",
+                    "part of speech": "adjective",
+                    "example sentence": "I am sad to not learn"
+                },
+                {
+                    "word": "car",
+                    "definition": "a 4 wheel vehicle with a motor",
+                    "part of speech": "noun",
+                    "example sentence": "I am going to drive my car to work"
+                }
+            }
+
+    // Add each of the 4 bits of data about the word to the Dictionary
+    excitedWord.Add("word", "excited");
+    excitedWord.Add("definition", "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness");
+    excitedWord.Add("part of speech", "adjective");
+    excitedWord.Add("example sentence", "I am excited to learn C#");
+
+    excitedWord.Add("word", "sad");
+    excitedWord.Add("definition", "just feelin down, man");
+    excitedWord.Add("part of speech", "adjective");
+    excitedWord.Add("example sentence", "I am sad to not learn");
+
+    excitedWord.Add("word", "car");
+    excitedWord.Add("definition", "a 4 wheel vehicle with a motor");
+    excitedWord.Add("part of speech", "noun");
+    excitedWord.Add("example sentence", "I am going to drive my car to work");
+
+
+
+
+            // Add Dictionary to your `dictionaryOfWords` list
+            dictionaryOfWords.Add(excitedWord);
+
+
+            // create another Dictionary and add that to the list
+            Dictionary<string, string> newDictionary = new Dictionary<string, string>()
+            {
+                {"word", "Hummer"},
+                {"definition", "A huge vehicle that burns gas"},
+                {"part of speech", "Pronoun"},
+                {"example sentence", "I drive a terrible vehicle"}
+            };
+
+            /*
+                Iterate your list of dictionaries and output the data, You can use the two foreach() loops below to help you start your iteration.
+
+                Example output for one word in the list of dictionaries:
+                    word: excited
+                    definition: having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness
+                    part of speech: adjective
+                    example sentence: I am excited to learn C#!
+            */
+
+            // Iterate the List of Dictionaries
+            foreach ()
+            {
+                // Iterate the KeyValuePairs of the Dictionary
+                foreach ()
+                {
+                    Console.WriteLine($"{wordData.Key}: {wordData.Value}");
+                }
+            }
         }
     }
 }
